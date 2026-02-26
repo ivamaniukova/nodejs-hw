8c13be3b4cabddb479ff277e1e5804afad7382f3
ivamaniukova <imaniukova@gmail.com>
2026-02-25 20:53:41 -0800

    deleted extra spaces

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
     const mongoUrl = process.env.MONGO_URL;
     await mongoose.connect(mongoUrl);
-    console.log('✅ MongoDB connection established successfully');
-
-    } catch (error) {
-  console.error('❌ Failed to connect to MongoDB:', error);
-  process.exit(1);
-}};
+    console.log("✅ MongoDB connection established successfully");
+  } catch (error) {
+    console.error("❌ Failed to connect to MongoDB:", error);
+    process.exit(1);
+  }
+};
 
 export { connectMongoDB };

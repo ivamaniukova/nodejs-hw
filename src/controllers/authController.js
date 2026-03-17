64c0d30b6ff626ef06a4cfbf8b6f3f5fdd547ed4
ivamaniukova <imaniukova@gmail.com>
2026-03-16 21:36:33 -0700

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,7 @@ import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
 import jwt from 'jsonwebtoken';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 import handlebars from 'handlebars';
 import fs from 'node:fs/promises';
 import path from 'path';
